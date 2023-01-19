d528d3547e1cb3b93ad849a601c7287014a8e5aa
greeshma <greeshma.jayaraj@maisondauraine.com>
2023-01-19 14:38:26 +0530

    GBA:1818 added popular search filter by subcategories

---
+++ app/code/Auraine/PopularSearch/registration.php
@@ -0,0 +1,8 @@
+<?php
+/**
+ * @author Pradeep M
+ */
+
+use \Magento\Framework\Component\ComponentRegistrar;
+
+ComponentRegistrar::register(ComponentRegistrar::MODULE, 'Auraine_PopularSearch', __DIR__);
