0591991ef6dd428a9d9b6ec4c34c533b22acafc6
greeshma <greeshma.jayaraj@maisondauraine.com>
2023-01-24 14:23:40 +0530

    GBA:2035 added patch data setup for adding Ladakh in RegionList

---
+++ app/code/Auraine/RegionList/Setup/Patch/Data/AddData.php
@@ -0,0 +1,69 @@
+<?php
+namespace Auraine\RegionList\Setup\Patch\Data;
+
+use Magento\Directory\Model\RegionFactory;
+use Magento\Framework\Setup\Patch\DataPatchInterface;
+use Magento\Framework\Setup\ModuleDataSetupInterface;
+
+class AddData implements DataPatchInterface
+{
+    /**
+     * @var ModuleDataSetupInterface
+     */
+    protected $moduleDataSetup;
+
+    /**
+     * @var RegionFactory
+     */
+    protected $_regionFactory;
+      /**
+       * @param ModuleDataSetupInterface $moduleDataSetup
+       * @param RegionFactory $regionFactory
+       */
+    public function __construct(
+        ModuleDataSetupInterface $moduleDataSetup,
+        RegionFactory $regionFactory
+    ) {
+        $this->moduleDataSetup = $moduleDataSetup;
+        $this->_regionFactory = $regionFactory;
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function apply()
+    {
+        $newRegions = [
+            'LA' => 'Ladakh'
+        ];
+
+        foreach ($newRegions as $code => $name) {
+
+            $binds = ['country_id'   => 'IN', 'code' => $code, 'default_name' => $name];
+            $this->moduleDataSetup->getConnection()->insert($this->moduleDataSetup->
+                getTable('directory_country_region'), $binds);
+            $regionId = $this->moduleDataSetup->getConnection()->
+                lastInsertId($this->moduleDataSetup->getTable('directory_country_region'));
+
+            $binds = ['locale'=> 'hi_IN', 'region_id' => $regionId, 'name'=> $name];
+            $this->moduleDataSetup->getConnection()->insert($this->moduleDataSetup->
+                getTable('directory_country_region_name'), $binds);
+        }
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public static function getDependencies()
+    {
+        return [];
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function getAliases()
+    {
+        return [];
+    }
+}
