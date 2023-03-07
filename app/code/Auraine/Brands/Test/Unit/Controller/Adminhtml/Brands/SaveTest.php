8b43071f8fd677a8acab9db734dcd0410c380acd
greeshma <greeshma.jayaraj@maisondauraine.com>
2023-03-07 11:37:40 +0530

    @codingStandardsIgnoreFile added

--- app/code/Auraine/Brands/Test/Unit/Controller/Adminhtml/Brands/SaveTest.php
+++ app/code/Auraine/Brands/Test/Unit/Controller/Adminhtml/Brands/SaveTest.php
@@ -7,7 +7,7 @@ use PHPUnit\Framework\MockObject\MockObject;
 
 /**
  * @covers \\Auraine\Brands\Controller\Adminhtml\Brands\Save
- * 
+ * @codingStandardsIgnoreFile
  */
 class SaveTest extends TestCase
 {
