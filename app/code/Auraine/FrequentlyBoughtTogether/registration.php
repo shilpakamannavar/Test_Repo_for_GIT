3f6774265c19e82335a7b64aa9fe3d22eeb25a69
greeshma <greeshma.jayaraj@maisondauraine.com>
2023-03-14 21:12:32 +0530

    GBA:3565 unit test cases added for frequentlt bought together, module nme changed from product recomender to FrequentlyBoughtTogether

--- app/code/Auraine/FrequentlyBoughtTogether/registration.php
+++ app/code/Auraine/FrequentlyBoughtTogether/registration.php
@@ -1,6 +1,6 @@
 <?php
 \Magento\Framework\Component\ComponentRegistrar::register(
     \Magento\Framework\Component\ComponentRegistrar::MODULE,
-    'Auraine_ProductRecomender',
+    'Auraine_FrequentlyBoughtTogether',
     __DIR__
 );
