cf7f18e660ea01b42327ea2e59b4394538605ef0
Shiffin <shiffin.thomas@maisondauraine.com>
2023-03-17 12:40:33 +0530

    code ignore removed

--- app/code/Auraine/TransactionalEmail/registration.php
+++ app/code/Auraine/TransactionalEmail/registration.php
@@ -3,9 +3,9 @@
  * Copyright ©  All rights reserved.
  * See COPYING.txt for license details.
  */
-// @codeCoverageIgnoreStart
+
 use Magento\Framework\Component\ComponentRegistrar;
 
 ComponentRegistrar::register(ComponentRegistrar::MODULE, 'Auraine_TransactionalEmail', __DIR__);
-// @codeCoverageIgnoreEnd
+
 
