fd0fc3b0ed7f1068b30153a4081a6d364a06ce87
Shiffin <shiffin.thomas@maisondauraine.com>
2023-03-17 12:30:36 +0530

    ignore all phtml files from code coverage

--- app/code/Auraine/TransactionalEmail/registration.php
+++ app/code/Auraine/TransactionalEmail/registration.php
@@ -3,7 +3,9 @@
  * Copyright ©  All rights reserved.
  * See COPYING.txt for license details.
  */
+// @codeCoverageIgnoreStart
 use Magento\Framework\Component\ComponentRegistrar;
 
 ComponentRegistrar::register(ComponentRegistrar::MODULE, 'Auraine_TransactionalEmail', __DIR__);
+// @codeCoverageIgnoreEnd
 
