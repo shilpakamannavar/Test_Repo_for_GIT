d187dfdb4d4823c6053fbf21ae7f2fac1103d4c8
akansha.rajput <akansha.rajput@maisondauraine.com>
2023-03-17 17:27:20 +0530

    Added code Coverage for ImageUploader

--- app/code/Auraine/ImageUploader/Test/Unit/Model/ResourceModel/Image/CollectionTest.php
+++ app/code/Auraine/ImageUploader/Test/Unit/Model/ResourceModel/Image/CollectionTest.php
@@ -1,57 +1,5 @@
 <?php
 
-// namespace Auraine\ImageUploader\Test\Unit\Model\ResourceModel\Image;
-
-// use Auraine\ImageUploader\Model\Image;
-// use Auraine\ImageUploader\Model\ResourceModel\Image as ResourceModelImage;
-// use Auraine\ImageUploader\Model\ResourceModel\Image\Collection;
-// use Magento\Framework\TestFramework\Unit\Helper\ObjectManager;
-// use PHPUnit\Framework\TestCase;
-// use Magento\Framework\App\ResourceConnection;
-// use Magento\Framework\DB\Adapter\AdapterInterface;
-
-// class CollectionTest extends TestCase
-// {
-//     /**
-//      * @var Collection
-//      */
-//     protected $collection;
-
-//     /**
-//      * @inheritDoc
-//      */
-//     protected function setUp(): void
-//     {
-//         $objectManager = new ObjectManager($this);
-    
-//         $this->collection = $objectManager->getObject(
-//             Collection::class,
-//             [
-//                 'connection' => $objectManager->getObject(AdapterInterface::class),
-//                 'imageModel' => $objectManager->getObject(Image::class),
-//                 'resourceModel' => $objectManager->getObject(ResourceModelImage::class),
-//             ]
-//         );
-//     }
-    
-
-//     /**
-//      * Test collection initialization
-//      */
-//     // public function testCollectionInitialization()
-//     // {
-//     //     $this->assertInstanceOf(Collection::class, $this->collection);
-//     // }
-
-//     public function test_Construct()
-//     {
-//         $collection = new Collection;
-
-//         $collection->create
-//     }
-// }
-
-
 namespace Auraine\ImageUploader\Test\Unit\Model\ResourceModel\Image;
 
 use Auraine\ImageUploader\Model\ResourceModel\Image\Collection;
